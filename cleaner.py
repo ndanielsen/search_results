187aa16ba6e23e81cdad963229f7ec3633676e7a
Nathan Danielsen <nathanjdanielsen@gmail.com>
2015-06-20 13:34:20 -0400

    setting framework up

---
+++ cleaner.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+"""
+Data Cleaning file for Auto_Ensemble 0.2
+
+by Nathan Danielsen
+nathanjdanielsen@gmail.com
+"""
+
+
+#### Text cleaning utilities
+
+### Lower
+### Remove stop words
+### Correct mispellings
+### Remove HTML / Markup
+
+
+#### Extract Features
+
+### First Four sentences
+### extract nouns
+### extract adjectives
+
+
+#### NLP Analysis
+
+### Compare to see if search ngram is in title or first four sentences
+### Use thesarsus of words and swap out for words in ngram
+### 
+
+
+
+
+
+
+
+class ClassName(object):
+	"""docstring for ClassName"""
+	def __init__(self, arg):
+		super(ClassName, self).__init__()
+		self.arg = arg
+		
